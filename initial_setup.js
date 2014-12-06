1e27755b2c604db5c139effaff4c06a5cf9f6e2f
Marcus Longmuir <marcuslongmuir@me.com>
2014-12-06 11:34:45 +0000

    Restructured and now working from empty db

--- initial_setup.js
+++ initial_setup.js
@@ -1,22 +1,23 @@
 var logger = require('tracer').console();
 
-module.exports = function(sdk){
+module.exports = function(sdk, minoval, done){
+
 	sdk.save_type({
 		"name":"contact_form",
 		"display_name":"Contact Form",
 		"type":"object",
 		"fields":[
 			{
-				"name":"body",
-				"display_name":"Body",
-				"type":"text"
-			},{
 				"name":"email",
 				"display_name":"Email",
 				"type":"email"
 			},{
 				"name":"subject",
 				"display_name":"Subject",
+				"type":"text"
+			},{
+				"name":"message",
+				"display_name":"Message",
 				"type":"text"
 			},{
 				"name":"notes",
@@ -29,31 +30,45 @@ module.exports = function(sdk){
 			}
 		]
 	}, function(err, res){
-		logger.log(err, res);
-	});
+		logger.log(JSON.stringify(err,null,4), res);
 
-	sdk.save([{
-		"name": "emails",
-		"folder": true,
-		"path": "/testuser/"
-	},{
-		"name": "cms",
-		"folder": true,
-		"path": "/testuser/"
-	}], function(err, res){
-		logger.log(err, res);
-
-		sdk.save([{
-			"name": "homepage",
-			"path": "/testuser/cms/",
-			"mino_user": {
-				"username": "THIS IS INITIAL",
-				"email": "t@t.com",
-				"salted_password": "SALTEDPASSWORDFTW",
-				"password_salt": "PASSWORDSALTFTW"
+		minoval.save_endpoint("contact_form", {
+			"contact_form" :{
+				"email": true,
+				"subject": true,
+				"message": true,
+				"notes": false,
+				"finished": false
 			}
-		}], function(err, res){
-			logger.log(err, res);
+		}, function(err, res){
+			logger.log(JSON.stringify(err,null,4), res);
+
+			sdk.save([{
+				"name": "emails",
+				"folder": true,
+				"path": "/testuser/"
+			},{
+				"name": "cms",
+				"folder": true,
+				"path": "/testuser/"
+			}], function(err, res){
+				logger.log(JSON.stringify(err,null,4), res);
+
+				sdk.save([{
+					"name": "homepage",
+					"path": "/testuser/cms/",
+					"mino_user": {
+						"username": "THIS IS INITIAL",
+						"email": "t@t.com",
+						"salted_password": "SALTEDPASSWORDFTW",
+						"password_salt": "PASSWORDSALTFTW"
+					}
+				}], function(err, res){
+					logger.log(JSON.stringify(err,null,4), res);
+
+					if(done) done();
+				});
+			});
 		});
-	})
+	});
 }
--- server.js
+++ server.js
@@ -2,54 +2,64 @@ var logger = require('tracer').console();
 var express = require('express');
 var sendgrid  = require('sendgrid')("bestan", "C2sEws3FIUdEK83GBD5a");
 
-var minoval = new (require('minoval'))({user: "testuser"})
 var mino = new (require('minodb'))({
     ui: true,
-    db_address: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/minodb_tests'
+    db_address: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/mino-example'
 })
-.add_plugin(minoval)
-.add_plugin(new (require('minocms'))({
-	"folder_name": "cms",
-	"user": "testuser"
-}))
-
 var sdk = new (require('minosdk'))("testuser").set_local_api(mino.api);
-require('./initial_setup')(sdk);
+var minoval = new (require('minoval'))({user: "testuser"});
 
-express()
-.use(require('errorhandler')())
-.use(require('body-parser')())
-.use(express.static('./public'))
-.use('/mino/', mino.server())
-.post("/send_email", function(req, res) {
-	minoval.validate("contact_form", req.body, function(validator) {
-		var error = validator.end();
-		if (error) {
-			return res.json(error)
-		}
-		req.body.contact_form.finished = false;
-		req.body.contact_form.notes = "Sent";
-		sdk.save([{
-            "name": req.body.contact_form.email + "_" + req.body.contact_form.subject + "_" + new Date().getTime(),
-            "path":"/testuser/emails/",
-            "contact_form": req.body.contact_form
-        }], function(error, response) {
-	    	if (error) {
-	    		return res.json(error)
-	    	} else {
-	    		sendgrid.send({
-					to: 'marcus@minocloud.com',
-					from: req.body.contact_form.email,
-					subject: req.body.contact_form.subject,
-					text: req.body.contact_form.message
-	    		}, function(err, json) {
-	    		 	if (err) { 
-	    		 		logger.error(err)
-	    		 		return res.json(err);
+sdk.create_user({
+	"username": "testuser",
+	"email": "marcus+test@minocloud.com",
+	"password": "my_password"
+}, function(err, res){
+	logger.log(JSON.stringify(err,null,4), res);
+
+	mino.add_plugin(new (require('minocms'))({
+		"folder_name": "cms",
+		"user": "testuser"
+	}))
+	.add_plugin(minoval, function(){
+		require('./initial_setup')(sdk, minoval, function(){
+
+			express()
+			.use(require('errorhandler')())
+			.use(require('body-parser')())
+			.use(express.static('./public'))
+			.use('/mino/', mino.server())
+			.post("/send_email", function(req, res) {
+				minoval.validate("contact_form", req.body, function(validator) {
+					var error = validator.end();
+					if (error) {
+						return res.json(error)
 					}
-					res.json({success:true})
-	    		});
-	    	}
+					req.body.contact_form.finished = false;
+					req.body.contact_form.notes = "Sent";
+					sdk.save([{
+			            "name": req.body.contact_form.email + "_" + req.body.contact_form.subject + "_" + new Date().getTime(),
+			            "path":"/testuser/emails/",
+			            "contact_form": req.body.contact_form
+			        }], function(error, response) {
+				    	if (error) {
+				    		return res.json(error)
+				    	} else {
+				    		sendgrid.send({
+								to: 'marcus@minocloud.com',
+								from: req.body.contact_form.email,
+								subject: req.body.contact_form.subject,
+								text: req.body.contact_form.message
+				    		}, function(err, json) {
+				    		 	if (err) { 
+				    		 		logger.error(err)
+				    		 		return res.json(err);
+								}
+								res.json({success:true})
+				    		});
+				    	}
+					});
+				});
+			}).listen(process.env.PORT || 5002);
 		});
-	});
-}).listen(process.env.PORT || 5002);
+	})
+});
